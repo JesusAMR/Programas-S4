356b49751152279c77812909d6bb4ef05d022b00
JesusM <jesusmiranda_6@hotmail.com>
2017-03-11 13:57:45 -0600

    De funcion a clase

--- Enteros.py
+++ Enteros.py
@@ -2,42 +2,33 @@
 
 import math
 
-f1 = open('Romanos.txt', 'r')
-f2 = open('Numeros.txt', 'w')
+romanosD =	{ "X":	1, "C":	2, "M":	3, "I":	0	}
 
-romanosD =	{
-						"X":	1,
-						"C":	2,
-						"M":	3,
-						"I":	0
-						}
+romanosC =	{ "V":	0, "L":	1, "D":	2	}
 
-romanosC =	{
-						"V":	0,
-						"L":	1,
-						"D":	2
-						}
+entrada = raw_input("Inserte el numero: ")
+class Decimales:
 
-#entrada = raw_input("Inserte el numero: ")
-for line in f1:
-	entrada = line  
-	long	= len(entrada)
-	sum 	= 0
-	i 		= 0
-	while(i != long):
-		if(i+1 < long and entrada[i+1] in romanosD and entrada[i] in romanosD and romanosD[entrada[i]] < romanosD[entrada[i+1]]):
-			sum = sum + pow(10, romanosD[entrada[i+1]]) - pow(10, romanosD[entrada[i]])
+	def __init__(self):
+		self.sum = 0
+	def calcular(self, inpu):
+		entrada = inpu  
+		long	= len(entrada)
+		sum 	= 0
+		i 		= 0
+		while(i != long):
+			if(i+1 < long and entrada[i+1] in romanosD and entrada[i] in romanosD and romanosD[entrada[i]] < romanosD[entrada[i+1]]):
+				sum = sum + pow(10, romanosD[entrada[i+1]]) - pow(10, romanosD[entrada[i]])
+				i = i + 1
+			elif(i+1 < long and entrada[i+1] in romanosC and entrada[i] in romanosD and romanosD[entrada[i]]== romanosC[entrada[i+1]]):
+				sum = sum + pow(10, romanosC[entrada[i+1]]) * 5 - pow(10, romanosD[entrada[i]])
+				i = i + 1
+			elif(entrada[i] in romanosD):
+				sum = sum + pow(10, romanosD[entrada[i]])
+			elif(entrada[i] in romanosC):
+				sum = sum + pow(10, romanosC[entrada[i]]) * 5
 			i = i + 1
-		elif(i+1 < long and entrada[i+1] in romanosC and entrada[i] in romanosD and romanosD[entrada[i]]== romanosC[entrada[i+1]]):
-			sum = sum + pow(10, romanosC[entrada[i+1]]) * 5 - pow(10, romanosD[entrada[i]])
-			i = i + 1
-		elif(entrada[i] in romanosD):
-			sum = sum + pow(10, romanosD[entrada[i]])
-		elif(entrada[i] in romanosC):
-			sum = sum + pow(10, romanosC[entrada[i]]) * 5
-		i = i + 1
+		return sum
 
-	f2.write(str(sum) + '\n')
-f1.close()
-f2.close()
-
+x = Decimales()
+print(x.calcular(entrada))
